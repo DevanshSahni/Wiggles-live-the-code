66df41bf4cbb3d6ab659d6e70be8e8fe78b2498a
reach-anu <reachanuuu@gmail.com>
2023-09-08 18:38:25 +0530

    add vaccination

--- client/src/Components/Vaccination.js
+++ client/src/Components/Vaccination.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from "./Navbar"
+import ShareVaccination from "./ShareVaccinations"
 import "../CSS/Vaccination.css"
 import Logo from "../images/wigglesLogo.png"
+import { BsShareFill } from 'react-icons/bs'
 
 const Vaccination = () => {
+
+    const[show, setShow]=useState(0);
+    const[addVaccination,setAddVaccination]=useState(false);
+    const handleClick = () =>{
+        setAddVaccination(!addVaccination);
+    }
+    const handleAddVaccine =(e)=>{
+        e.preventDefault();
+        console.log("hi im working");
+    }
   return (
     <>
         <Navbar/>
         <div className='vaccinationWrapper'>
+        <div className="shareIconContainer"><BsShareFill className="shareIcon" onClick={()=>show ? setShow(0):setShow(1)}/></div>
+        <ShareVaccination show={show}/>
             <div className='headerContainer'>
                 <div className='logoInfoContainer'>
                     <img src={Logo} alt="website-logo"></img>
@@ -35,15 +49,24 @@ const Vaccination = () => {
                 <div className='vaccinationContainer'>
                     <div className='vaccinationInfoPrimary'>
                         <h1>Vaccinations</h1>
-                        <button id="addVaccination">Add +</button>
+                        <button id="addVaccination" form="vaccinationForm" onClick={handleClick}>{addVaccination ? "Save" : "Add +"}</button>
                     </div>
+                    <form id="vaccinationForm" onSubmit={handleAddVaccine}></form>
                     <table>
                         <tr>
                         <th>Name</th>
                         <th>Batch Number</th>
-                        <th>Date of vaccination</th>
-                        <th>Due date</th>
+                        <th>Date</th>
+                        <th>Next visit</th>
                         </tr>
+                        {addVaccination && 
+                        <tr className='addVaccinationForm'>
+                                <td><input type="text" placeholder="Name" form="vaccinationForm" /></td>
+                                <td><input type="text" placeholder="Batch number" form="vaccinationForm"/></td>
+                                <td><input  placeholder="Date" form="vaccinationForm"/></td>
+                                <td><input  placeholder="Next Visit" form="vaccinationForm"/></td>
+                        </tr>
+                        }
                     </table>
                 </div>
             </div>
