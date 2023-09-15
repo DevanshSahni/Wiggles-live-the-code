f76fc584cbc89291218a6357cb9d18b7759316c3
Devansh Sahni <devanshsahni@gmail.com>
2023-09-16 01:21:36 +0530

    test domain name

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -27,7 +27,7 @@ module.exports.Login = async (req, res, next)=>{
               maxAge:1000*60*60*24*3, 
               withCredentials: true,
               httpOnly: false,
-              domain: wiggles-live-the-code.vercel.app,
+              domain: .vercel.app,
               secure: true,
               sameSite:'none',
           });
@@ -35,7 +35,7 @@ module.exports.Login = async (req, res, next)=>{
               maxAge:1000*60*60*24*3, 
               withCredentials: true,
               httpOnly: false,
-              domain: wiggles-live-the-code.vercel.app,
+              domain: .vercel.app,
               secure: true,
               sameSite:'none',
           });
